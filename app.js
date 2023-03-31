7b8fcd648518f86f91ba4281f7a70dcaba7cd148
inaya27 <inayaasif161@gmail.com>
2023-03-31 11:03:20 -0700

    web code push on github

---
+++ app.js
@@ -0,0 +1,273 @@
+// var name = "Mark";
+
+// name = "Ace";
+
+alert("Thanks for your input!");
+
+var title ="Thanks for your input"
+console.log(title)
+
+
+
+// ----------chapter 2-------------
+
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+console.log(lastName + cityOfOrigin + aussieGreeting)
+
+var nickname = "Bub";
+console.log(nickname)
+
+
+var nationality;
+nationality = "U.S.";
+console.log(nationality)
+
+
+
+var name = "Mark";
+var nickname = "Bub";
+var floogle = "Mark";
+console.log(name)
+console.log(nickname)
+console.log(floogle)
+
+
+
+
+var lessonAuthor = "Mark";
+var guyWhoKeepsSayingHisOwnName = "Mark";
+var x = "Mark";
+
+var thanx = "Thanks for your input!"
+alert(thanx);
+
+// ----------chapter 3--------------
+
+var originalNum = 23;
+ var newNum = originalNum + 7;
+
+ var originalNumber = 23;
+ var newNumber = originalNumber + 7;
+ console.log(newNumber)
+
+
+ var weight = 150;
+ weight + 25
+
+ var originalNum = 23;
+ var numToBeAdded = 7;
+ var newNum = originalNum + numToBeAdded;
+ console.log(newNum)
+
+ var originalNum = 90;
+ originalNum = originalNum + 10;
+ 
+
+ var originalNum = "23";
+ var newNum = originalNum + 7;
+ console.log(newNum)
+
+ 
+
+ alert(144);
+ var caseQty = 144;
+ alert(caseQty);
+
+
+//  ----------chapter 4-----------
+    var abc = "abc" //legal
+    var abc12 = "abc" //legal
+    var $abc = "abc" //legal
+
+    // var 123 = "abc" //illegal
+    // var !@#$%*() = "abc" //ilegal
+    // var 12ab = "abc" //illegal
+
+    // ------------chapter 5-------------
+    
+    var popularNumber = 4;
+    var popularNumber = 2 + 2;
+
+    var num = 10;
+    var popularNumber = num + 200;
+
+    var num = 10;
+    var anotherNum = 1;
+    var popularNumber = num + anotherNum;
+
+    // ------------chapter 6-------------
+
+    num = num + 1;
+
+    var num = 1;
+    var newNum = num++;
+
+
+    var num = 1;
+     var newNum = ++num;
+     console.log(newNum)
+
+     
+
+    var num = 1;
+     var newNum = --num;
+     console.log(newNum)
+
+
+     var num = 1;
+var newNum = --num;
+console.log(newNum)
+console.log(num)
+
+
+    // ---------------chapter 7-------------
+
+    var totalCost = 1 + (3 * 4);
+
+    var totalCost = (1 + 3) * 4;
+
+    var resultOfComputation = (2 * 4) * 4 + 2;
+
+
+    var totalCost = 1 + 3 * 4;
+console.log(totalCost)
+
+
+
+// ----------------chapter 8--------------
+
+    var message = "Thanks for your input!";
+    alert(message);
+
+    var message = "Thanks, ";
+     var banger = "!";
+    alert(message + userName + banger);
+
+    var message = "Thanks, ";
+     var userName = "Susan";
+     var banger = "!";
+    var customMess = message + userName + banger;
+    alert(customMess);
+
+    alert("2 plus 2 equals " + 2 + 2);
+
+    // -----------------chapter 9--------------
+
+    var firstname = prompt("enter first name")
+    var lastname = prompt("enter last name")
+    var city = prompt("enter city")
+    console.log(firstname  + " " + lastname + " from " + city)
+
+     var question = "Your species?";
+    var defaultAnswer = "human";
+    var spec = prompt(question, defaultAnswer);
+
+    var numberOfCats = prompt("How many cats?");
+     var tooManyCats = numberOfCats + 1;
+
+     var question = "Your species?";
+var defaultAnswer = "human";
+var spec = prompt(question, defaultAnswer);
+
+    // -----------------chapter 10---------------
+
+    // var x = prompt("Where does the Pope live?");
+    //  if (x === "Vatican") (
+    //  alert("Correct!");
+    //  )
+
+
+     var correctAnswer = "Vatican";
+     if (x === correctAnswer) {
+      alert("Correct!");
+      }
+
+      var correctAnswer = "Vatican";
+ if (x === correctAnswer) {
+ score++;
+ userIQ = "genius";
+ alert("Correct!");
+ }
+
+
+
+//  --------------chapter 11------------------
+
+// if (fullName === "Mark" + " " + "Myers") 
+//     if (fullName === firstName + " " + "Myers") 
+//     if (fullName === firstName + " " + "Myers") 
+//     if (fullName === "firstName + " " + lastName) 
+//     if (totalCost === 81.50 + 135) 
+//     if (totalCost === materialsCost + 135) 
+//     if (totalCost === materialsCost + laborCost) 
+//     if (x + y === a - b) 
+    
+yourTicketNumber = box
+         if (yourTicketNumber !== 487208) {
+             alert("Better luck next time.");
+             }
+
+             if (1 > 0) 
+                if (0 < 1) 
+                if (1 >= 0) 
+                
+
+                if (1 >= 1) 
+                if (0 <= 1) 
+                if (1 <= 1) 
+
+
+        // ----------------chapter 12---------------
+
+
+        var x = prompt("Where does the Pope live?");
+         if (x === "Vatican") {
+        alert("Correct!");
+         }
+
+
+         var x = prompt("Where does the Pope live?");
+     if (x === "Vatican") {
+    alert("Correct!");
+ }
+    if (x !== "Vatican") {
+     alert("Wrong answer");
+ }
+
+
+    if (x === "Vatican") {
+         alert("Correct!");
+     }
+        else {
+        alert("Wrong answer");
+         }
+
+
+         var correctAnswer = "Vatican";
+     if (x === correctAnswer) {
+    alert("Correct!");
+ }
+    else {
+    score--;
+    userIQ = "problematic";
+     alert("Incorrect");}
+    
+
+     var correctAnswer = "Vatican";
+      if (x === correctAnswer) {
+     
+      alert("Correct!");
+      }
+      else if (x === "Rome") {
+      alert("Incorrect but close");
+      }
+      else {
+      alert("Incorrect");
+     }
+
+
+
+
+                
